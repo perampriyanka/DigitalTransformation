e03ec37e4c901aa40b5406034bb9270baf43ff76
perampriyanka <peram.priyanka@gmail.com>
2016-06-16 07:57:41 +0000

    Add files via upload

---
+++ ShoppingCart/src/main/java/com/niit/shoppingcart/test/TestCategory.java
@@ -0,0 +1,25 @@
+package com.niit.shoppingcart.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.shoppingcart.bean.Category;
+
+public class TestCategory {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
+		context.scan("com.niit.shoppingcart.bean");
+		context.refresh();
+		
+      Category p=(Category) context.getBean("category");
+      if(p==null)
+      {
+    	  System.out.println("unable to create object");
+      }
+      else
+      {
+    	  System.out.println("Category object is created");
+      }
+	}
+
+}
